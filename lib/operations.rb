531aa30a5ddd751a0cf299a5b71cb577d2314ee0
mahdesignstudio <mahdesignstudio@gmail.com>
2020-05-30 17:13:28 -0400

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,30 @@
 def unsafe?(speed)
-
+    if speed > 60 
+        true
+    elsif speed < 40
+        true
+    else 
+        false
+    end 
 end
 
 
 
 def not_safe?(speed)
-	
+    speed < 40 || speed > 60 ? true : false 
 end
+
+    # Code below is part of getting each operation passed *
+    # if speed > 60
+    #     true
+    # elsif speed < 40 
+    #     true
+    # else 
+    #     false 
+    # end 
+     
+
+
 	
 
 
